14e281699dd060e45bfa04cec2f47e2c6be2a4c1
Mike Young <MichealYoung5309@gmail.com>
2021-03-12 00:48:08 -0500

    Fixed a bug

--- src/ch379/Main.java
+++ src/ch379/Main.java
@@ -1,18 +1,25 @@
 package ch379;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double income = 1234567;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter Income: ");
+		String incomeString = in.nextLine();
+		in.close();
+		
+		double income = Integer.parseInt(incomeString);
+		double TaxAmmount = Math.round(TaxCalc(income) * 100.00) / 100.00;
 		
 		System.out.print("Income: ");
 		System.out.println(income);
 		System.out.print("Tax: ");
-		System.out.println(TaxCalc(income));
+		System.out.println(TaxAmmount);
 		System.out.print("Take home: ");
-		System.out.println(income-TaxCalc(income));
+		System.out.println(income-TaxAmmount);
 		
 	}
 	
@@ -23,14 +30,13 @@ public class Main {
 		double TaxAmmt = 0;
 		for(int i = 1; i < Brackets[1].length; i++)
 		{
-			if (income > Brackets[0][i])
+			if (income > Brackets[0][i] && Brackets[0][i] != 0)
 				{
 					Taxable[i-1] =  (Brackets[0][i] - Brackets[0][i-1]) * Brackets[1][i];
-					income = income-Brackets[0][i];
 				}
 			else
 			{
-				Taxable[i-1] = income * Brackets[1][i];
+				Taxable[i-1] = (income-Brackets[0][i-1]) * Brackets[1][i];
 				break;
 			}
 		}
@@ -41,12 +47,14 @@ public class Main {
 		
 		return TaxAmmt;
 	}
-	
+	//Returns a 2d array
+	// TaxBrackets[0] = Bracket maximum
+	// TaxBrackets[1] = Bracket rate
 	static double[][] getBrackets()
 	{
 		double[][] TaxBrackets = {
 			{0,10000,30000,100000,0},
-			{0,0,0.1,0.25,0.4}
+			{0,0.0,0.1,0.25,0.4}
 		};
 		return TaxBrackets;
 	}
